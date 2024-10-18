a3aacff893346563dc91886dbbbf736ad5e32f09
xleo0002 <111990883+xleo0002@users.noreply.github.com>
2024-10-18 16:26:17 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// var vg_1 = "pledge_map.vg.json";
+// var vg_2 = "population_bar.vg.json";
+// var viz = "viz.vg.json";
+// var vg_3 = "ethnicity_pie_chart.vg.json";
+
+// vegaEmbed("#proportional_symbol_map", vg_1).then(function (result) {
+// }).catch(console.error);
+
+// vegaEmbed("#bar_chart", vg_2).then(function (result) {
+// }).catch(console.error);
+
+// vegaEmbed("#viz", viz).then(function (result) {
+// }).catch(console.error);
+
+// vegaEmbed("#pie_chart", viz).then(function (result) {
+// }).catch(console.error);
+
+var map = "js/map.vg.json";
+var donut = "js/donut.vg.json"
+var bar = "js/bar.vg.json"
+var bump = "js/bump.vg.json"
+var multibar = "js/multibar.vg.json"
+
+var viz_1 = "js/viz_1.vg.json";
+
+vegaEmbed("#map", map).then(function (result) {
+}).catch(console.error);
+
+vegaEmbed("#donut", donut).then(function (result) {
+}).catch(console.error);
+
+vegaEmbed("#bar", bar).then(function (result) {
+}).catch(console.error);
+
+vegaEmbed("#bump", bump).then(function (result) {
+}).catch(console.error);
+
+vegaEmbed("#multibar", multibar).then(function (result) {
+}).catch(console.error);
+
+vegaEmbed("#viz_1", viz_1).then(function (result) {
+}).catch(console.error);
+
+
